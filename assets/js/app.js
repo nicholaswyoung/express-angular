321b831764a6d7fc648db3d1d2f85a54f570ec05
Nicholas Young <nicholas@nicholaswyoung.com>
2014-05-01 15:24:01 -0500

    Updating database records now works.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,54 @@
 //= require angular
 //= require angular.resource
+//= require angular.route
 
-var App = window.App = angular.module('testApp', ['ngResource'])
-,   PostCtrl;
+var App = window.App = angular.module('testApp', ['ngResource', 'ngRoute'])
+,   PostCtrl, PostDetailCtrl;
 
-App.config
+App.factory('Post', [
+  '$resource',
+  function ($resource) {
+    return $resource('posts/:id', { id: '@id' }, {
+      update: { method: 'PUT' }
+    });
+  }
+]);
 
-App.factory('Post', ['$resource', function ($resource) {
-  return $resource('posts/:id', { id: '@id' }, {
-    update: { method: 'PUT' }
-  });
+App.controller('PostCtrl', [
+  '$scope',
+  'Post',
+  function ($scope, Post) {
+    $scope.posts = Post.query();
+
+    $scope.addPost = function () {
+      this.post = Post.save($scope.newPost);
+      $scope.posts.push(this.post);
+      $scope.newPost = {};
+    };
+  }
+]);
+
+App.controller('PostDetailCtrl', [
+  '$scope',
+  '$routeParams',
+  'Post',
+  function ($scope, $routeParams, Post) {
+    $scope.post = Post.get({ id: $routeParams.id });
+
+    $scope.savePost = function () {
+      $scope.post.$update();
+    }
+  }
+]);
+
+App.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'PostCtrl',
+      templateUrl: 'templates/posts/index'
+    })
+    .when('/posts/:id', {
+      templateUrl: 'templates/posts/show',
+      controller: 'PostDetailCtrl'
+    })
 }]);
-
-PostCtrl = ['$scope', 'Post', function ($scope, Post) {
-  $scope.posts = Post.query();
-
-  $scope.addPost = function () {
-    this.post = Post.save($scope.newPost);
-    $scope.posts.push(this.post);
-    $scope.newPost = {};
-  };
-}];
